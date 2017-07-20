97e88cd2ad6c607bdf3b3caf1596b7c4061e2a1d
garrettbsettles <garrettbsettles@gmail.com>
2017-07-20 09:03:02 -0400

    garrett added event listener for button

--- main.js
+++ main.js
@@ -7,3 +7,12 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+//the following deals with the search bar and submit button event listeners
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+
+button.addEventListener('submit', function(event){
+  //this is where you tie the api in
+  event.preventDefault()
+})
