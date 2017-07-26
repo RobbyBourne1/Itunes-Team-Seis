94b7e8e33a76eedfe7e9ce9ba572f83d68476741
Cody Clark <c.susie.run@gmail.com>
2017-07-26 08:45:43 -0400

    Audio Player Changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let displayOfArtists = document.querySelector('.search-info')
-let audioPlayer = document.querySelector('.music-player')
+// let audioPlayer = document.querySelector('.music-player')
 let searchBar = document.querySelector('.search-bar')
 let audioSource = document.querySelector('audio')
 const maxDescLength = 12
@@ -12,63 +12,63 @@ let url = 'https://itunes.apple.com/search?term='
 input.autofocus = 'on'
 
 let searchInput = query => {
-  // Search Bar Funtionality
-  // console.log(input.value)
+    // Search Bar Funtionality
+    // console.log(input.value)
 
-  // Completing the search
-  let searchUrl = url + query + '&limit=20'
-  displayOfArtists.innerHTML = ''
+    // Completing the search
+    let searchUrl = url + query + '&limit=20'
+    displayOfArtists.innerHTML = ''
 
-  fetch(searchUrl).then(response => response.json()).then(artist => {
-    artist.results.forEach(function(artistData, index) {
-      console.log(artistData)
+    fetch(searchUrl).then(response => response.json()).then(artist => {
+        artist.results.forEach(function(artistData, index) {
+            console.log(artistData)
 
-      const gallery = document.createElement('li')
-      const artistsLink = document.createElement('a')
-      const artistsImg = document.createElement('img')
-      const artistAndTrack = document.createElement('div')
-      const artistsTitle = document.createElement('span')
-      const trackName = document.createElement('span')
-      // Replacing pic URL's px size and pulling Pics
-      artistsImg.src = artistData.artworkUrl100.replace('100x100', '500x500')
-      // Pulling aritist name
-      artistsTitle.textContent = artistData.artistName
+            const gallery = document.createElement('li')
+            const artistsLink = document.createElement('a')
+            const artistsImg = document.createElement('img')
+            const artistAndTrack = document.createElement('div')
+            const artistsTitle = document.createElement('span')
+            const trackName = document.createElement('span')
+            // Replacing pic URL's px size and pulling Pics
+            artistsImg.src = artistData.artworkUrl100.replace('100x100', '500x500')
+            // Pulling aritist name
+            artistsTitle.textContent = artistData.artistName
 
-      //Setting Titles/Names to a certain length
-      const name =
-        artistData.artistName.length > maxDescLength
-          ? (artistsTitle.textContent = artistData.artistName.slice(0, maxDescLength) + '...')
-          : (artistsTitle.textContent = artistData.artistName)
+            //Setting Titles/Names to a certain length
+            const name =
+                artistData.artistName.length > maxDescLength
+                    ? (artistsTitle.textContent = artistData.artistName.slice(0, maxDescLength) + '...')
+                    : (artistsTitle.textContent = artistData.artistName)
 
-      const title =
-        artistData.trackName.length > maxDescLength
-          ? (trackName.textContent = artistData.trackName.slice(0, maxDescLength) + '...')
-          : (trackName.textContent = artistData.trackName)
+            const title =
+                artistData.trackName.length > maxDescLength
+                    ? (trackName.textContent = artistData.trackName.slice(0, maxDescLength) + '...')
+                    : (trackName.textContent = artistData.trackName)
 
-      //Media Player Functionality
-      artistsLink.addEventListener('click', event => {
-        audioSource.setAttribute('src', artistData.previewUrl)
-        audioSource.play()
-      })
+            //Media Player Functionality
+            artistsLink.addEventListener('click', event => {
+                audioSource.setAttribute('src', artistData.previewUrl)
+                audioSource.play()
+            })
 
-      // Appending Things to page
-      displayOfArtists.appendChild(gallery)
-      gallery.appendChild(artistsLink)
-      artistsLink.appendChild(artistsImg)
-      artistsImg.setAttribute('alt', artistData.trackName)
-      artistsLink.appendChild(artistAndTrack)
-      artistAndTrack.appendChild(artistsTitle)
-      artistAndTrack.appendChild(trackName)
+            // Appending Things to page
+            displayOfArtists.appendChild(gallery)
+            gallery.appendChild(artistsLink)
+            artistsLink.appendChild(artistsImg)
+            artistsImg.setAttribute('alt', artistData.trackName)
+            artistsLink.appendChild(artistAndTrack)
+            artistAndTrack.appendChild(artistsTitle)
+            artistAndTrack.appendChild(trackName)
+        })
     })
-  })
 }
 
 form.addEventListener('submit', event => {
-  event.stopPropagation()
-  event.preventDefault()
-  searchInput(search)
+    event.stopPropagation()
+    event.preventDefault()
+    searchInput(search)
 })
 
 input.addEventListener('keyup', event => {
-  searchInput(input.value)
+    searchInput(input.value)
 })
