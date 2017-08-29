28282365c9c0d9e255f5d9fa29da5d9af61fc861
Robby Bourne <robbybourne1@gmail.com>
2017-08-29 18:04:23 -0400

    Fixed commneted code

--- main.js
+++ main.js
@@ -168,15 +168,12 @@ const processArtists = artists => {
     artistAndTrack.appendChild(trackName)
     artistAndTrack.appendChild(faveButton)
     //
-    // favoriteButton.addEventListener('click', event => {
-    //   // console.log(favoriteQuery)
     // })
   })
 }
 
 let searchInput = query => {
-  // Search Bar Funtionality
-  // console.log(input.value)
+
 
   // Completing the search
   let searchUrl = url + query + '&limit=20'
@@ -202,35 +199,3 @@ form.addEventListener('submit', event => {
 input.addEventListener('keyup', event => {
   searchInput(input.value)
 })
-
-// searchButton.addEventListener("click", function(){
-//     let searchTerm = search.value
-//     const promise =
-// let url = 'https://itunes.apple.com/search?term=jack+johnson'
-// fetch(url).then(response => response.json()).then(artist => {
-//     artist.results.forEach(function(artistData, index) {
-//         console.log(artistData)
-//     })
-// })
-// const gallery = document.createElement('li')
-// const artistsLink = document.createElement('a')
-// const artistsThumbnail = document.createElement('img')
-// const spanDiv = document.createElement("div")
-// const artistsTitle = document.createElement("span")
-// const trackName = document.createElement("span")
-// artistsLink.href = artistsLink.href
-// artistsTitle.textContent = artistData.artistTitle
-// trackName.textContent = trackName.trackName
-// artistsThumbnail.src = artistsThumbnail.artworkUrl60
-// gallery.appendChild(artistsLink)
-// gallery.appendChild(artistsTitle)
-// gallery.appendChild(recipeThumbnail)
-// // recipeLink.appendChild(recipeThumbnail)
-// // gallery.appendChild(recipeIngredients)
-// gallery.appendChild(recipeLink)
-// displayOfRecipes.appendChild(gallery)
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
